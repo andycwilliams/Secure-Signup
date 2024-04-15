6914fa4ed3e7e5b7da9bf8054388569821182b04
Andy <awilliamscomposing@gmail.com>
2024-04-15 15:15:18 -0700

    Initializing front and back ends

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 8001;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, console.log(`Now listening on PORT: ${PORT}`));
